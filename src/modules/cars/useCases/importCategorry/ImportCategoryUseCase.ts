ea90cb6a84279ae3fdf3ddce6bae69a78a43cedb
Paulo Xavier <pxavierf@gmail.com>
2021-09-29 12:45:34 -0300

    initial implements stream

--- src/modules/cars/useCases/importCategorry/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategorry/ImportCategoryUseCase.ts
@@ -1,7 +1,20 @@
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import parse from 'csv-parse';
 
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file)
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async (line) => {
+      console.log('oi')
+      console.log(line)
+    })
   }
 }
 
